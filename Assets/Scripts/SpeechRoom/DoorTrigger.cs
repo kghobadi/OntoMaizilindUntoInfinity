1e3689401ea7068eb74cedf25a60fa73a88c7ba7
Kas <ksg304@nyu.edu>
2020-06-30 18:41:10 -0400

    lots of apartment decorations. i have been forced to add png to LFS....

---
+++ Assets/Scripts/SpeechRoom/DoorTrigger.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorTrigger : AudioHandler {
+
+    Animator doorAnimator;
+
+    [Header("Sounds")]
+    public AudioClip doorClose;
+    public AudioClip doorOpen;
+
+    void Start()
+    {
+        doorAnimator = GetComponent<Animator>();
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "Human")
+        {
+            Debug.Log("person entered " + gameObject.name);
+            if(doorAnimator.GetBool("opened") == false)
+            {
+                doorAnimator.SetBool("opened", true);
+
+                PlaySoundRandomPitch(doorOpen, 1f);
+            }
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "Human")
+        {
+            if (doorAnimator.GetBool("opened") == true)
+            {
+                doorAnimator.SetBool("opened", false);
+
+                PlaySoundRandomPitch(doorClose, 1f);
+            }
+            Debug.Log("person exited " + gameObject.name);
+        }
+    }
+}
