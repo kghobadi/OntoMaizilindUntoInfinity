52767d516e4da57037cd1611d2fcedbbafadc6f2
kghobadi <kasghobadi@gmail.com>
2023-02-20 18:08:08 -0500

    made it easier to position NPCs with editor scripts allowing you to snap them all to ground.
    - moved all Block 1 citizens to correct positions. running pretty well!

---
+++ Assets/Editor/NpcMovementEditor.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NPC;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(Movement))]
+public class NpcMovementEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        //Get planet man script
+        Movement npcMovement = (Movement)target;
+
+        //button for snapping NPC to ground 
+        if (GUILayout.Button("Snap NPC to Ground"))
+        {
+            npcMovement.SnapToGroundPoint();
+        }
+
+        base.OnInspectorGUI();
+    }
+}
---
+++ Assets/Editor/NpcMovementMgrEditor.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NPC;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(NPCMovementManager))]
+public class NpcMovementMgrEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        //Get planet man script
+        NPCMovementManager npcMovementMgr = (NPCMovementManager)target;
+
+        //button for finding all Npc controllers. 
+        if (GUILayout.Button("Get All NPC Controllers"))
+        {
+            npcMovementMgr.FindAllNPCs();
+        }
+
+        //button for snapping All NPC to ground 
+        if (GUILayout.Button("Snap All NPCs to Ground"))
+        {
+            npcMovementMgr.SnapAllNPCsToGround();
+        }
+
+        base.OnInspectorGUI();
+    }
+}
