2481696ab5bec0a296dc219a54b6b4600c117594
kghobadi <kasghobadi@gmail.com>
2023-05-05 15:24:08 -0400

    made train sound 'dont destroy on load'

---
+++ Assets/Scripts/General/DontDestroyOnLoad.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DontDestroyOnLoad : MonoBehaviour
+{
+    //Simply makes an obj persist between scenes.
+    void Start()
+    {
+        DontDestroyOnLoad(gameObject);
+    }
+}
