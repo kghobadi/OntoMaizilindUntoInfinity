8cd860233365c5456a68d54c0e76281a1b6b4237
yulele_palacios <yulelepalacios@gmail.com>
2024-06-09 18:47:51 +0200

    ramin apartment crime scene

---
+++ Assets/Materials/Apartment/Ramin's Apartment/ConstantRotation.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ConstantRotation : MonoBehaviour {
+
+	void Start () {
+	
+	}
+	public bool X;
+	public bool Y;
+	public bool Z;
+
+
+	public float Xvalue;
+	public float Yvalue;
+	public float Zvalue;
+
+	void Update () {
+		if (Time.timeScale == 0) { return; }
+		if (X) {
+			transform.Rotate (Xvalue * Time.deltaTime, 0, 0); //rotates 50 degrees per second around z axis
+		}
+		if (Y) {
+			transform.Rotate ( 0, Yvalue * Time.deltaTime, 0); //rotates 50 degrees per second around z axis
+		}
+		if (Z) {
+			transform.Rotate (0, 0, Zvalue * Time.deltaTime); //rotates 50 degrees per second around z axis
+		}
+	}
+}
