6d39fa51105a5018ad25eda517c5b3dadd19ef38
kghobadi <ksg304@nyu.edu>
2025-07-14 22:41:52 -0400

    implemented ramiel audio for amins battle

---
+++ Assets/Scripts/Triggers/OnAudioClipFinish.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Transition audio source when it finishes something. 
+/// </summary>
+public class OnAudioClipFinish : AudioHandler
+{
+    [SerializeField] 
+    private bool setNewClipOnFinish;
+    [SerializeField] 
+    private AudioClip newClip;
+    [SerializeField] 
+    private bool setLoop;
+    
+    void Start()
+    {
+        InvokeRepeating("CheckFinished", 1f, 1f);
+    }
+
+
+    void CheckFinished()
+    {
+        if ( myAudioSource.isPlaying == false)
+        {
+            SetFinishedBehaviors();
+        }
+    }
+
+    void SetFinishedBehaviors()
+    {
+        if (setNewClipOnFinish)
+        {
+            myAudioSource.clip = newClip;
+        }
+
+        if (setLoop)
+        {
+            myAudioSource.loop = true;
+        }
+        myAudioSource.Play();
+    }
+}
